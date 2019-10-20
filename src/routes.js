236a1e682b24573e51a3395e7871d0377fe17c0b
unknown <guilhermekuni@gmail.com>
2019-10-20 15:56:42 -0300

    implements user autheantication middleware

--- src/routes.js
+++ src/routes.js
@@ -3,11 +3,14 @@ import { Router } from 'express';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 
+import authMiddleware from './app/middlewares/auth';
+
 const routes = new Router();
 
 // USERS
 routes.get('/users',  UserController.index);
 routes.post('/users', UserController.store);
+routes.put('/users', authMiddleware, UserController.update);
 
 // SESSIONS
 routes.post('/sessions', SessionController.store);
