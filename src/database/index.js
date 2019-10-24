aaae89b5907cbc1d0a18c319751966ae6e695368
unknown <guilhermekuni@gmail.com>
2019-10-24 19:32:22 -0300

    create file table migration and file model

--- src/database/index.js
+++ src/database/index.js
@@ -1,10 +1,11 @@
 import Sequelize from 'sequelize';
 
 import User from '../app/models/User';
+import File from '../app/models/File';
 
 import databaseConfig from '../config/database';
 
-const models = [User];
+const models = [User, File];
 
 class Database {
   constructor() {
@@ -18,4 +19,3 @@ class Database {
 }
 
 export default new Database();
-
