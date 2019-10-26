c2bd7118726173725ae7d3b50565a2336c74f199
unknown <guilhermekuni@gmail.com>
2019-10-26 13:20:47 -0300

    creates Appointment model and db migration

--- src/database/index.js
+++ src/database/index.js
@@ -2,10 +2,11 @@ import Sequelize from 'sequelize';
 
 import User from '../app/models/User';
 import File from '../app/models/File';
+import Appoinment from '../app/models/Appoinment';
 
 import databaseConfig from '../config/database';
 
-const models = [User, File];
+const models = [User, File, Appoinment];
 
 class Database {
   constructor() {
