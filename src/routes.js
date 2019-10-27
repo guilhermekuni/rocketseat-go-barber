f7998a49f37a311ed78061ef8bb568c3682e63ec
unknown <guilhermekuni@gmail.com>
2019-10-27 10:24:59 -0300

    implements notification GET endpoint

--- src/routes.js
+++ src/routes.js
@@ -6,6 +6,7 @@ import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
+import NofiticationController from './app/controllers/NofiticationController';
 
 import authMiddleware from './app/middlewares/auth';
 import AppointmentController from './app/controllers/AppointmentController';
@@ -28,6 +29,8 @@ routes.get('/appointments', AppointmentController.index);
 routes.post('/appointments', AppointmentController.store);
 
 routes.get('/schedule', ScheduleController.index);
+
+routes.get('/notifications', NofiticationController.index);
 
 routes.post('/files', upload.single('file'), FileController.store);
 
