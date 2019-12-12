0a8c5691768b353ba05026d4db6e2a1874145271
Guilherme Kuniyoshi <guilhermekuni@gmail.com>
2019-12-12 20:25:58 -0300

    configura react navigation

--- frontend-mobile/android/app/src/main/java/com/mobile/MainActivity.java
+++ frontend-mobile/android/app/src/main/java/com/mobile/MainActivity.java
@@ -3,6 +3,9 @@ package com.mobile;
 import com.facebook.react.ReactActivity;
 
 public class MainActivity extends ReactActivity {
+  import com.facebook.react.ReactActivityDelegate;
+  import com.facebook.react.ReactRootView;
+  import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
 
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
@@ -11,5 +14,15 @@ public class MainActivity extends ReactActivity {
   @Override
   protected String getMainComponentName() {
     return "mobile";
+  }
+
+  @Override
+  protected ReactActivityDelegate createReactActivityDelegate() {
+    return new ReactActivityDelegate(this, getMainComponentName()) {
+      @Override
+      protected ReactRootView createRootView() {
+       return new RNGestureHandlerEnabledRootView(MainActivity.this);
+      }
+    };
   }
 }
