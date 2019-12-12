2b0a17feff80e9b24e2819997e711ca6839d08a5
Guilherme Kuniyoshi <guilhermekuni@gmail.com>
2019-12-12 20:34:21 -0300

    fix MaiActivity.java

--- frontend-mobile/android/app/src/main/java/com/mobile/MainActivity.java
+++ frontend-mobile/android/app/src/main/java/com/mobile/MainActivity.java
@@ -1,12 +1,11 @@
 package com.mobile;
 
 import com.facebook.react.ReactActivity;
+import com.facebook.react.ReactActivityDelegate;
+import com.facebook.react.ReactRootView;
+import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
 
 public class MainActivity extends ReactActivity {
-  import com.facebook.react.ReactActivityDelegate;
-  import com.facebook.react.ReactRootView;
-  import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
-
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
