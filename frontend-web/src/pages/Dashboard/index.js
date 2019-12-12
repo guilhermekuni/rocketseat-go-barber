9133bf7716a19ca58a5589c2346d93c62fd19223
Guilherme Kuniyoshi <guilhermekuni@gmail.com>
2019-12-11 22:21:47 -0300

    implements dashboard page

--- frontend-web/src/pages/Dashboard/index.js
+++ frontend-web/src/pages/Dashboard/index.js
@@ -1,12 +1,41 @@
 import React from 'react';
 import api from '~/services/api';
 
-import Profile from '../Profile';
+import { MdChevronLeft, MdChevronRight } from 'react-icons/md';
 
-// import { Container } from './styles';
+import { Container, Time } from './styles';
 
 export default function Dashboard() {
   return (
-    <Profile />
+    <Container>
+      <header>
+        <button type="button">
+          <MdChevronLeft size={36} color="#fff" />
+        </button>
+        <strong>31 de Maio</strong>
+        <button type="button">
+          <MdChevronRight size={36} color="#fff" />
+        </button>
+      </header>
+
+      <ul>
+        <Time past>
+          <strong>08:00</strong>
+          <span>Guilherme Kuniyoshi</span>
+        </Time>
+        <Time available>
+          <strong>09:00</strong>
+          <span>Em aberto</span>
+        </Time>
+        <Time>
+          <strong>10:00</strong>
+          <span>Guilherme Kuniyoshi</span>
+        </Time>
+        <Time>
+          <strong>11:00</strong>
+          <span>Guilherme Kuniyoshi</span>
+        </Time>
+      </ul>
+    </Container>
   );
 }
