0c5e5c27cb35bf83bdfb8c7dc068f0f5ac42d3c8
Guilherme Kuniyoshi <guilhermekuni@gmail.com>
2019-12-12 21:46:46 -0300

    implements SignIn page

--- frontend-mobile/src/pages/SignIn/index.js
+++ frontend-mobile/src/pages/SignIn/index.js
@@ -1,18 +1,41 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { Image } from 'react-native';
+
+import logo from '~/assets/logo.png';
 
 import Background from '~/components/Background';
-import Input from '~/components/Input';
-import Button from '~/components/Button';
 
-// import { Container } from './styles';
+import {
+  Container, Form, FormInput, SubmitButton, SignLink, SignLinkText,
+} from './styles';
 
 export default function SignIn() {
   return (
     <Background>
-      <Text>SignIn</Text>
-      <Input style={{ marginTop: 30 }} icon="call" placeholder="Digite seu nome" />
-      <Button>Entrar</Button>
+      <Container>
+        <Image source={logo} />
+
+        <Form>
+          <FormInput
+            icon="mail-outline"
+            keyboardType="email-address"
+            autoCorrect={false}
+            autoCaptalize="none"
+            placeholder="Digite seu e-mail"
+          />
+          <FormInput
+            icon="lock-outline"
+            secureTextEntry
+            placeholder="Digite sua senha secreta"
+          />
+
+          <SubmitButton onPress={() => {}}>Acessar</SubmitButton>
+        </Form>
+
+        <SignLink>
+          <SignLinkText>Criar conta gratuita</SignLinkText>
+        </SignLink>
+      </Container>
     </Background>
   );
 }
